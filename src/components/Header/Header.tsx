a0f244fb10d2f8775f750c092e78346cfb829bfd
René Opitz <rop@xima.de>
2025-12-05 00:08:01 +0100

    scroll effect

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,11 @@
 import { FunctionComponent } from 'preact'
 import './Header.css'
-
 import { Logo } from '@components/Logo';
-// import { DarkmodeToggle } from '@components/DarkmodeToggle';
-// import { Icon } from '../Icon';
-// import Button from '../Button/Button';
-// import { Dialog } from '../Dialog';
-// import { useState } from 'preact/compat';
-// import { ColorPicker } from '../ColorPicker';
 
 const Header: FunctionComponent = () => {
     return (
         <header class="header">
-            {/* <div class="header__content"> */}
-                <Logo />
-            {/* </div> */}
+            <Logo />
         </header>
     )
 }
